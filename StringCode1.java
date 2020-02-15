0c3dd860865f0f7ffe61740d0aff8fdf168c2d0f
gunjalharsh <41323871+gunjalharsh@users.noreply.github.com>
2020-02-15 12:35:44 +0530

    Add files via upload

--- StringCode1.java
+++ StringCode1.java
@@ -8,18 +8,20 @@ public class StringCode1 {
 	Scanner str=new Scanner(System.in);
 	System.out.println("Enter the String :");
 	String input=str.nextLine();
-
-		for(char i :input.toCharArray())
-		{		
-			
-		if(input.indexOf(i)==input.lastIndexOf(i))
+	char[] ch =input.toCharArray();
+	
+	for(int i=0;i<ch.length;i++)
+	{
+		char c=input.charAt(i);
+		if(input.indexOf(c)==input.lastIndexOf(c))
 		{
-			
-		System.out.println(i);
-		break;
-		}
+			System.out.println(input.charAt(i));
+			System.out.println("Index Location is :"+input.indexOf(c));
+			break;
 		}
 		
+	}
+			
 	}
 
 }
